8f60bda1f0f7e24c6d4de9d4ba5cb331b7eb65ac
Andrew Quardex <megasounds@mail.ru>
2020-01-22 20:44:26 +0300

    v.0.0.29

--- qcore/QSource.php
+++ qcore/QSource.php
@@ -8,39 +8,6 @@ class QSource
     protected $_connectedParams = false;
     private $__get;
 
-    private function getPubKey() {
-        return "-----BEGIN PUBLIC KEY-----\r\n".
-        "MIIFIjANBgkqhkiG9w0BAQEFAAOCBQ8AMIIFCgKCBQEA5E0SODOlpOfKlhVzr+yO\r\n".
-        "96o0d21bhMKW4ohBhixLkmPoWsC53MiHdJXM5fZpvyUESXY0gmI712BtoibhzGke\r\n".
-        "o0IaRU7KErQERv80cbzjtI7ZsAQASVly1jUnhzVNgrcL1N/kyUMk8aLgX1SkcUcT\r\n".
-        "PzqrvuC1/SshHi3O7Af644YdsC5FFkM5ANo7pCpsvGX5NUXhMAY4lzLItEwRBb6Z\r\n".
-        "YP18cLQv18LTmTEKWPTA6/O7dpGghKr/OyWeP1pJ57L8JYIxBrmVcsQHKxv4X6TV\r\n".
-        "HHEWzsmxpvplDAmDQaJIL20gR+YbGh2uqHqhY7zgpNQVjtAG/ePoX+va3P9SKzF0\r\n".
-        "YaLfSGbXPGIgWexfNNq94KeDqZpBLGm7a6fa3qzbrjEgPsMJuwczHhE9ROgR8e94\r\n".
-        "Etk06JuksQd85/fUYCMQEu4t/p5LCFh0yWGVm2RxzsdIwZJZKM2sMz0tiquxITER\r\n".
-        "fEfrqyKkMsYLCn/WjAZRKg4rwXwsuxy1xiBwFRG+DzMFAQQzBFRiplcoHBmOWjRW\r\n".
-        "A2EV/Cgcg624qSuS3hjN0E1aw6c0Fq89uYftq+OreuPK4DXK99uh6Vf5Gt+BdYb/\r\n".
-        "dfZ4ULQajdHoB3y7sJ60QuPUynH0BgIzbIlTMO+QeLq7oBEWGxwIW7tswTDI+qns\r\n".
-        "Sgs1/UoUeOC65b8/lXqkh6w7yJmjAduP7k3TTmZE6D7LRd2yifDcf8AFpsUyHWoy\r\n".
-        "cmCjw3ydG1s9qYrdz9T6BfE2F7CGCpu5V5EI+DcQMmdqYeQ9SR3kn4n0qJJFESXj\r\n".
-        "OO/bMB7yyxMJqnzmtn9+QCOSuJqN8CwUFeqU2ZNod6/3BbKmbAHLSeBJ4GnYsEZp\r\n".
-        "YDX2K251wcx6GqMMPfUu/gnZXVVlAZH2xZb5oHfrgIbsr7+68MKKH3+HZ7VCEqli\r\n".
-        "P2Nf9OXNNWdzCi0L6bsJNI9IGbRP7yvgJYLj4syQLHFJOTtYWZPymIjmyI0mPcAg\r\n".
-        "G9LYdNJ1xaxVVC6ySaV0OybpNjTNSHNAZNwWrWr/S4s6fJ+eqxnILDr58lxGHnWy\r\n".
-        "/jXbf2mKxwG7ov/0ek3g1DXl4WxpfxpkLhjkxgzp2CDuGzibKnZd16NXGMTMmh/o\r\n".
-        "dPDZUS4ikcDV983nKKNE0D3YZR9WHlYaDPrReihfIX0dKlKUFv3IibOeD/bcyE4a\r\n".
-        "32whYeQvM9yDbj0Uaz6yt16d9FILEnuj5LhrxnnBmqd/GpHyiLCpghFGT09aLH5R\r\n".
-        "9aUeZBdlGwWeG0G6U8g9ejVtOparDad6RPdnQDm66qFli/ZcvrBhxVr6QVaEPspt\r\n".
-        "ejSzuOCMt2qp0BiROM/bZtkAt6JF52+ebLmED9tGxGZI4b2RhT2ONaxs3i27ufCB\r\n".
-        "GYR5UKZ9wDkgy8s0JUquWc4n8kS5pzK4+L+O1kfuGv6NnObhX0RfEFrV2yU8o9sf\r\n".
-        "qLtxQDuQfI/OSi4rogHUbgzDfZ+HSrJI8USlVjtvZjd1Et8XkKsfcF4xHJ8ZDKA8\r\n".
-        "MqO4tJDec61/vzXYzQbUWtlHfeqQhg8XMTBeg2vhDyp99Uv3R2RV2txgcwu2nue4\r\n".
-        "ZODGYHpPBV522ZMZf0Se0ctBNkRM4ogv5BEvt5e1pfwfZQ4AQ9F7ZpHnLVtIWtGp\r\n".
-        "kEg8ngwjIrHNHM0ygE11Tv2XWPisXdkByNPTYkXN9lE6lLvEl5I6kBvYTUliPHCw\r\n".
-        "AvjyXhntZ7hNNwSaTcZDkk8CAwEAAQ==\r\n".
-        "-----END PUBLIC KEY-----";
-    }
-
     protected function connectParams($params)
     {
         $this->_connectedParams = [];
@@ -124,6 +91,39 @@ class QSource
     public function __call($closure, $args)
     {
         throw new \Exception('Calling unknown method: ' . get_class($this) . "::$name()");
+    }
+
+    private function getPubKey() {
+        return "-----BEGIN PUBLIC KEY-----\r\n".
+            "MIIFIjANBgkqhkiG9w0BAQEFAAOCBQ8AMIIFCgKCBQEA5E0SODOlpOfKlhVzr+yO\r\n".
+            "96o0d21bhMKW4ohBhixLkmPoWsC53MiHdJXM5fZpvyUESXY0gmI712BtoibhzGke\r\n".
+            "o0IaRU7KErQERv80cbzjtI7ZsAQASVly1jUnhzVNgrcL1N/kyUMk8aLgX1SkcUcT\r\n".
+            "PzqrvuC1/SshHi3O7Af644YdsC5FFkM5ANo7pCpsvGX5NUXhMAY4lzLItEwRBb6Z\r\n".
+            "YP18cLQv18LTmTEKWPTA6/O7dpGghKr/OyWeP1pJ57L8JYIxBrmVcsQHKxv4X6TV\r\n".
+            "HHEWzsmxpvplDAmDQaJIL20gR+YbGh2uqHqhY7zgpNQVjtAG/ePoX+va3P9SKzF0\r\n".
+            "YaLfSGbXPGIgWexfNNq94KeDqZpBLGm7a6fa3qzbrjEgPsMJuwczHhE9ROgR8e94\r\n".
+            "Etk06JuksQd85/fUYCMQEu4t/p5LCFh0yWGVm2RxzsdIwZJZKM2sMz0tiquxITER\r\n".
+            "fEfrqyKkMsYLCn/WjAZRKg4rwXwsuxy1xiBwFRG+DzMFAQQzBFRiplcoHBmOWjRW\r\n".
+            "A2EV/Cgcg624qSuS3hjN0E1aw6c0Fq89uYftq+OreuPK4DXK99uh6Vf5Gt+BdYb/\r\n".
+            "dfZ4ULQajdHoB3y7sJ60QuPUynH0BgIzbIlTMO+QeLq7oBEWGxwIW7tswTDI+qns\r\n".
+            "Sgs1/UoUeOC65b8/lXqkh6w7yJmjAduP7k3TTmZE6D7LRd2yifDcf8AFpsUyHWoy\r\n".
+            "cmCjw3ydG1s9qYrdz9T6BfE2F7CGCpu5V5EI+DcQMmdqYeQ9SR3kn4n0qJJFESXj\r\n".
+            "OO/bMB7yyxMJqnzmtn9+QCOSuJqN8CwUFeqU2ZNod6/3BbKmbAHLSeBJ4GnYsEZp\r\n".
+            "YDX2K251wcx6GqMMPfUu/gnZXVVlAZH2xZb5oHfrgIbsr7+68MKKH3+HZ7VCEqli\r\n".
+            "P2Nf9OXNNWdzCi0L6bsJNI9IGbRP7yvgJYLj4syQLHFJOTtYWZPymIjmyI0mPcAg\r\n".
+            "G9LYdNJ1xaxVVC6ySaV0OybpNjTNSHNAZNwWrWr/S4s6fJ+eqxnILDr58lxGHnWy\r\n".
+            "/jXbf2mKxwG7ov/0ek3g1DXl4WxpfxpkLhjkxgzp2CDuGzibKnZd16NXGMTMmh/o\r\n".
+            "dPDZUS4ikcDV983nKKNE0D3YZR9WHlYaDPrReihfIX0dKlKUFv3IibOeD/bcyE4a\r\n".
+            "32whYeQvM9yDbj0Uaz6yt16d9FILEnuj5LhrxnnBmqd/GpHyiLCpghFGT09aLH5R\r\n".
+            "9aUeZBdlGwWeG0G6U8g9ejVtOparDad6RPdnQDm66qFli/ZcvrBhxVr6QVaEPspt\r\n".
+            "ejSzuOCMt2qp0BiROM/bZtkAt6JF52+ebLmED9tGxGZI4b2RhT2ONaxs3i27ufCB\r\n".
+            "GYR5UKZ9wDkgy8s0JUquWc4n8kS5pzK4+L+O1kfuGv6NnObhX0RfEFrV2yU8o9sf\r\n".
+            "qLtxQDuQfI/OSi4rogHUbgzDfZ+HSrJI8USlVjtvZjd1Et8XkKsfcF4xHJ8ZDKA8\r\n".
+            "MqO4tJDec61/vzXYzQbUWtlHfeqQhg8XMTBeg2vhDyp99Uv3R2RV2txgcwu2nue4\r\n".
+            "ZODGYHpPBV522ZMZf0Se0ctBNkRM4ogv5BEvt5e1pfwfZQ4AQ9F7ZpHnLVtIWtGp\r\n".
+            "kEg8ngwjIrHNHM0ygE11Tv2XWPisXdkByNPTYkXN9lE6lLvEl5I6kBvYTUliPHCw\r\n".
+            "AvjyXhntZ7hNNwSaTcZDkk8CAwEAAQ==\r\n".
+            "-----END PUBLIC KEY-----";
     }
 }
     
--- qcore/Quartronic.php
+++ qcore/Quartronic.php
@@ -83,7 +83,7 @@ class Quartronic extends QSource
   }
 
   function getVersion() {
-    return '0.0.28';
+    return '0.0.29';
   }
 }
 
